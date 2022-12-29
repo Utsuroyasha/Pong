d35b69a69b9159e620a0db41cb220978b51e2d80
Utsuroyasha <shiroyashah@gmail.com>
2022-12-29 03:53:01 -0600

    funtionality

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -34,7 +34,8 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     public void newBall() {
-
+        //random = new Random();
+        ball = new Ball((GAME_WIDTH / 2) - (BALL_DIAMETER / 2), (GAME_HEIGHT / 2) - (BALL_DIAMETER / 2), BALL_DIAMETER, BALL_DIAMETER);
     }
 
     public void newPaddles() {
@@ -52,13 +53,60 @@ public class GamePanel extends JPanel implements Runnable{
     public void draw(Graphics g) {
         paddle1.draw(g);
         paddle2.draw(g);
+        ball.draw(g);
     }
 
     public void move() {
-
+        paddle1.move();
+        paddle2.move();
+        ball.move();
     }
 
     public void checkCollision() {
+        //bounce ball off walls
+        if(ball.y <= 0) {
+            ball.setYDirection(-ball.yVelocity);
+        }
+        if(ball.y >= GAME_HEIGHT - BALL_DIAMETER) {
+            ball.setYDirection(-ball.yVelocity);
+        }
+        //bounces ball of paddles
+         if (ball.intersects(paddle1)) {
+             ball.xVelocity = Math.abs(ball.xVelocity);
+             ball.xVelocity++; //adds more difficulty
+             if (ball.yVelocity > 0) {
+                 ball.yVelocity++; //adds more difficulty
+             } else {
+                 ball.yVelocity--;
+             }
+             ball.setXDirection(ball.xVelocity);
+             ball.setYDirection(ball.yVelocity);
+         }
+        if (ball.intersects(paddle2)) {
+            ball.xVelocity = Math.abs(ball.xVelocity);
+            ball.xVelocity++; //adds more difficulty
+            if (ball.yVelocity > 0) {
+                ball.yVelocity++; //adds more difficulty
+            } else {
+                ball.yVelocity--;
+            }
+            ball.setXDirection(-ball.xVelocity);
+            ball.setYDirection(ball.yVelocity);
+        }
+        //stops paddles at the edge of window
+        if (paddle1.y <= 0) {
+            paddle1.y = 0;
+        }
+        if (paddle1.y >= (GAME_HEIGHT - PADDLE_HEIGHT)) {
+            paddle1.y = GAME_HEIGHT - PADDLE_HEIGHT;
+        }
+        if (paddle2.y <= 0) {
+            paddle2.y = 0;
+        }
+        if (paddle2.y >= (GAME_HEIGHT - PADDLE_HEIGHT)) {
+            paddle2.y = GAME_HEIGHT - PADDLE_HEIGHT;
+        }
+        
     }
 
     public void run() {
@@ -82,11 +130,14 @@ public class GamePanel extends JPanel implements Runnable{
 
     public class AL extends KeyAdapter {
         public void keyPressed(KeyEvent e) {
-
+        paddle1.keyPressed(e);
+        paddle2.keyPressed(e);
+        //repaint();
         }
 
         public void keyReleased (KeyEvent e) {
-
+        paddle1.keyReleased(e);
+        paddle2.keyReleased(e);
         }
     }
 }
